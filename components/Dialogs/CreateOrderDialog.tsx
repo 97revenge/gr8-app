9562b9694f4d632ff1cefa50739b659480d548e6
Matheus Pereira <empregos97@gmail.com>
2024-03-21 17:19:31 -0300

    Route (app)                              Size     First Load JS ┌ ○ /                                    36.1 kB         128 kB ├ ○ /_not-found                          882 B          85.3 kB ├ λ /api/budget                          0 B                0 B ├ λ /api/deleteBudget                    0 B                0 B ├ λ /api/deleteProduct                   0 B                0 B ├ λ /api/editBudget                      0 B                0 B ├ λ /api/editProduct                     0 B                0 B ├ ○ /api/order                           0 B                0 B ├ λ /api/products                        0 B                0 B ├ ○ /dashboard                           22.1 kB         167 kB ├ ○ /dashboard/budget                    2.41 kB         168 kB ├ ○ /dashboard/products                  4.83 kB         168 kB └ ○ /login                               5.3 kB         89.7 kB + First Load JS shared by all            84.4 kB   ├ chunks/69-e4d33f8c3bac93bb.js        29 kB   ├ chunks/fd9d1056-0fa3fc3f0dc55762.js  53.4 kB   └ other shared chunks (total)          2.02 kB
    ○  (Static)   prerendered as static content
    λ  (Dynamic)  server-rendered on demand using Node.js

--- components/Dialogs/CreateOrderDialog.tsx
+++ components/Dialogs/CreateOrderDialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useForm } from "react-hook-form";
 import {
   DialogContent,
@@ -23,6 +25,9 @@ import toast from "react-hot-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
+import { useState } from "react";
+import { MaterialSymbolsArrowCircleRight as Right } from "../Icons/MaterialSymbolsArrowCircleRight";
+import { MaterialSymbolsArrowBack as Back } from "../Icons/MaterialSymbolsArrowBack";
 
 const optionSchema = z.object({
   label: z.string(),
@@ -37,6 +42,14 @@ const OPTIONS: Option[] = [
   { label: "Outros", value: "Outros" },
 ];
 
+const contentSchema = z.object({
+  amount: z.string(),
+  unit: z.string(),
+  code: z.string(),
+  description: z.string(),
+  unit_price: z.string(),
+});
+
 const formSchema = z.object({
   client: z.string().min(2).max(50),
   date: z.string().min(1),
@@ -47,9 +60,12 @@ const formSchema = z.object({
     .max(11, { message: "Maximo de 11 caracteres" }),
   annotation: z.string().optional(),
   shop: z.array(optionSchema).min(1),
+  content01: contentSchema,
 });
 
 export default function CreateOrderDialog() {
+  const [view, setView] = useState<boolean>(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -58,11 +74,26 @@ export default function CreateOrderDialog() {
       email: "",
       number: "",
       annotation: "",
+      shop: [],
+      content01: {},
     },
   });
 
   const handler = async (data: z.infer<typeof formSchema>) => {
-    return alert(JSON.stringify(data));
+    try {
+      const instance: Partial<typeof data | any> = {
+        client: data.client,
+        date: data.date,
+        email: data.email,
+        number: data.number,
+        shop: String(data.shop[0].value),
+        content: [data.content01],
+      };
+
+      return alert(JSON.stringify(instance));
+    } catch (err) {
+      alert(err);
+    }
   };
 
   return (
@@ -77,135 +108,274 @@ export default function CreateOrderDialog() {
 
         <Form {...form}>
           <form action="post" onSubmit={form.handleSubmit(handler)}>
-            <FormField
-              control={form.control}
-              name="client"
-              render={({ field }) => (
-                <>
-                  <FormItem>
-                    <FormLabel>Nome do Cliente</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Insira o nome do cliente"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                </>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="date"
-              render={({ field }) => (
-                <div>
-                  <FormItem>
-                    <FormLabel>Data do pedido</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="insira a data do pedido"
-                        type="date"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                </div>
-              )}
-            />
+            {view == false ? (
+              <>
+                <FormField
+                  control={form.control}
+                  name="client"
+                  render={({ field }) => (
+                    <>
+                      <FormItem>
+                        <FormLabel>Nome do Cliente</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Insira o nome do cliente"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    </>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="date"
+                  render={({ field }) => (
+                    <div>
+                      <FormItem>
+                        <FormLabel>Data do pedido</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="insira a data do pedido"
+                            type="date"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    </div>
+                  )}
+                />
 
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <div>
-                  <FormItem>
-                    <FormLabel>E-mail do cliente</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="insira a data do pedido"
-                        type="email"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                </div>
-              )}
-            />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <div>
+                      <FormItem>
+                        <FormLabel>E-mail do cliente</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="insira a data do pedido"
+                            type="email"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    </div>
+                  )}
+                />
 
-            <FormField
-              control={form.control}
-              name="number"
-              render={({ field }) => (
-                <div>
-                  <FormItem>
-                    <FormLabel>Numero do Cliente</FormLabel>
-                    <FormControl>
-                      <Input
-                        defaultValue={"+55"}
-                        placeholder="insira a numero do cliente"
-                        type="text"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                </div>
-              )}
-            />
+                <FormField
+                  control={form.control}
+                  name="number"
+                  render={({ field }) => (
+                    <div>
+                      <FormItem>
+                        <FormLabel>Numero do Cliente</FormLabel>
+                        <FormControl>
+                          <Input
+                            defaultValue={"+55"}
+                            placeholder="insira a numero do cliente"
+                            type="text"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    </div>
+                  )}
+                />
 
-            <FormField
-              control={form.control}
-              name="annotation"
-              render={({ field }) => (
-                <div>
-                  <FormItem>
-                    <FormLabel>Anotações adicionais</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        placeholder="Digite anotações adicionais aqui..."
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                </div>
-              )}
-            />
+                <FormField
+                  control={form.control}
+                  name="annotation"
+                  render={({ field }) => (
+                    <div>
+                      <FormItem>
+                        <FormLabel>Anotações adicionais</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            placeholder="Digite anotações adicionais aqui..."
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    </div>
+                  )}
+                />
 
-            <FormField
-              control={form.control}
-              name="shop"
-              render={({ field }) => (
-                <div>
-                  <FormItem>
-                    <FormLabel>Anotações adicionais</FormLabel>
-                    <FormControl>
-                      <MultipleSelector
-                        value={field.value}
-                        onChange={field.onChange}
-                        defaultOptions={OPTIONS}
-                        maxSelected={1}
-                        onMaxSelected={() =>
-                          toast.error("Voçe só pode adicionar um.")
-                        }
-                        placeholder="Select frameworks you like..."
-                        emptyIndicator={
-                          <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
-                            no results found.
-                          </p>
-                        }
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                </div>
-              )}
-            />
+                <FormField
+                  control={form.control}
+                  name="shop"
+                  render={({ field }) => (
+                    <div>
+                      <FormItem>
+                        <FormLabel>Anotações adicionais</FormLabel>
+                        <FormControl>
+                          <MultipleSelector
+                            value={field.value}
+                            onChange={field.onChange}
+                            defaultOptions={OPTIONS}
+                            maxSelected={1}
+                            onMaxSelected={() =>
+                              toast.error("Voçe só pode adicionar um.")
+                            }
+                            placeholder="Selecione o provedor de pedido..."
+                            emptyIndicator={
+                              <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                                Sem mais resultados
+                              </p>
+                            }
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    </div>
+                  )}
+                />
 
-            <Button type="submit"> enviar</Button>
+                <div className="w-full flex items-center justify-center">
+                  <Right onClick={() => setView(!view)}></Right>
+                </div>
+
+                <Button type="submit" variant={"shine"} className="w-full">
+                  {" "}
+                  enviar
+                </Button>
+              </>
+            ) : (
+              <div>
+                Pedido 01
+                <div className="grid grid-cols-2  gap-4 lg:grid-cols-3 lg:gap-8 w-full  p-2">
+                  <div className="flex  items-center justify-center text-center ">
+                    <FormField
+                      control={form.control}
+                      name="content01.amount"
+                      render={({ field }) => (
+                        <>
+                          <FormItem>
+                            <FormLabel>
+                              <span className="font-bold text-md">
+                                Quantidade
+                              </span>{" "}
+                            </FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder=" Quantidade"
+                                {...field}
+                                className="w-32"
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        </>
+                      )}
+                    />
+                  </div>
+                  <div className="flex items-center justify-center text-center ">
+                    <FormField
+                      control={form.control}
+                      name="content01.unit"
+                      render={({ field }) => (
+                        <>
+                          <FormItem>
+                            <FormLabel>
+                              <span className="font-bold text-md">Unidade</span>{" "}
+                            </FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder=" Unidade"
+                                {...field}
+                                className="w-32"
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        </>
+                      )}
+                    />
+                  </div>
+                  <div className="flex items-center justify-center text-center ">
+                    <FormField
+                      control={form.control}
+                      name="content01.code"
+                      render={({ field }) => (
+                        <>
+                          <FormItem>
+                            <FormLabel>
+                              <span className="font-bold text-md">Código</span>{" "}
+                            </FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder=" Código"
+                                {...field}
+                                className="w-32"
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        </>
+                      )}
+                    />
+                  </div>
+                  <div className="flex items-center justify-center text-center ">
+                    <FormField
+                      control={form.control}
+                      name="content01.unit_price"
+                      render={({ field }) => (
+                        <>
+                          <FormItem>
+                            <FormLabel>
+                              <span className="font-bold text-md">
+                                Preço Unitario
+                              </span>{" "}
+                            </FormLabel>
+                            <FormControl>
+                              <Input
+                                placeholder=" Preço Unitario"
+                                {...field}
+                                className="w-32"
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        </>
+                      )}
+                    />
+                  </div>
+                  <div className="relative left-24 lg:left-0 flex items-center justify-start text-center w-full  ">
+                    <FormField
+                      control={form.control}
+                      name="content01.description"
+                      render={({ field }) => (
+                        <>
+                          <FormItem>
+                            <FormLabel>
+                              <span className="font-bold text-md">
+                                Descrição
+                              </span>{" "}
+                            </FormLabel>
+                            <FormControl>
+                              <Textarea
+                                placeholder="Descrição do seu pedido"
+                                {...field}
+                                className="w-[250px] p-2"
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        </>
+                      )}
+                    />
+                  </div>
+                </div>
+                <Back onClick={() => setView(!view)}></Back>
+              </div>
+            )}
           </form>
         </Form>
       </DialogContent>
